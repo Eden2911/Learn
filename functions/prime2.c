4f5845c2549e5c26a21ff14dde00dc76b57e1482
Edenity <edenitysukhlain@gmail.com>
2021-04-23 10:39:06 +0530

    modify prime2.c

--- functions/prime2.c
+++ functions/prime2.c
@@ -3,26 +3,25 @@ int prime(int num);
 main()
 {
 int num, check;
-printf("prime numbers:");
 printf("Enter the number to check: ");
 scanf("%d", &num);
 check=prime(num);
 
 if(check==1)
-    printf("The number is not prime.");
-else
-printf("The number is prime.");
+    printf("\nThe number is not prime.");
+if(check==0)
+printf("\n The number is prime.");
 }
 
 int prime(int num)
 {
     int i;
-    for (i=2; i<num; i++)
+    for (i=2; i<=num/2; i++)
     {
         if (num%i!=0)
         continue;
         else
             return(1);
     }
-
+return(0);
 }
